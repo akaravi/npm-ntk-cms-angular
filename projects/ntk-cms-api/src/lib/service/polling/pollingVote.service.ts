2a7355cb210a0efca420bc1b81de321b752bf81c
Alireza Karavi <home+akaravi@users.noreply.github.com>
2020-11-12 19:28:29 +0330

    PollingVote update

--- projects/ntk-cms-api/src/lib/service/polling/pollingVote.service.ts
+++ projects/ntk-cms-api/src/lib/service/polling/pollingVote.service.ts
@@ -5,9 +5,25 @@ import { PollingVoteModel } from '../../models/entity/polling/PollingVoteModel';
 import { ErrorExcptionResult } from '../../models/entity/base/errorExcptionResult';
 
 export class PollingVoteService extends ApiCmsServerBase<PollingVoteModel, number>  {
-
-
   getModuleCotrolerUrl(): string {
     return 'PollingVote';
+  }
+  ServiceAddBatch(
+    model: PollingVoteModel,
+  ): Observable<ErrorExcptionResult<PollingVoteModel>> {
+    if (model == null) {
+      model = new PollingVoteModel();
+    }
+    return this.http
+      .post(this.getBaseUrl() + this.getModuleCotrolerUrl() + '/AddBatch', model, {
+        headers: this.getHeaders(),
+      })
+      .pipe(
+        retry(this.configApiRetry),
+        // catchError(this.handleError)
+        map((ret: ErrorExcptionResult<PollingVoteModel>) => {
+          return this.errorExcptionResultCheck(ret);
+        }),
+      );
   }
 }
