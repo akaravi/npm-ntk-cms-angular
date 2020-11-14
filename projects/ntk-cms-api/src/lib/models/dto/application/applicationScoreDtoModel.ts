c3ca80a5b9232e5f3eb45eda188655f3fa4a2d76
karavi home <offic-karavi@ntk.ir>
2020-11-14 15:51:31 +0330

    mdoe update

---
+++ projects/ntk-cms-api/src/lib/models/dto/application/applicationScoreDtoModel.ts
@@ -0,0 +1,6 @@
+export class ApplicationScoreDtoModel {
+  LinkApiId: number;
+  AppMemberId: string;
+  ScoreComment: string;
+  ScorePercent: number;
+}
--- projects/ntk-cms-api/src/lib/service/application/applicationApp.service.ts
+++ projects/ntk-cms-api/src/lib/service/application/applicationApp.service.ts
@@ -1,12 +1,31 @@
+import { ApplicationScoreDtoModel } from '../../models/dto/application/applicationScoreDtoModel';
 import { ApplicationAppModel } from '../../models/entity/application/applicationAppModel';
 import { ApiCmsServerBase } from '../base/apiCmsServerBase.service';
+import { ErrorExcptionResultBase } from '../../models/entity/base/errorExcptionResultBase';
+import { Observable } from 'rxjs';
+import { map, retry } from 'rxjs/operators';
 
 
 
 export class ApplicationAppService extends ApiCmsServerBase<ApplicationAppModel, number>  {
-
-
   getModuleCotrolerUrl(): string {
     return 'ApplicationApp';
+  }
+
+  ServiceScoreClick(model: ApplicationScoreDtoModel): Observable<ErrorExcptionResultBase> {
+    if (model == null) {
+      model = new ApplicationScoreDtoModel();
+    }
+    return this.http
+      .post(this.getBaseUrl() + this.getModuleCotrolerUrl() + '/ScoreClick', model, {
+        headers: this.getHeaders(),
+      })
+      .pipe(
+        retry(this.configApiRetry),
+        // catchError(this.handleError)
+        map((ret: ErrorExcptionResultBase) => {
+          return this.errorExcptionResultBaseCheck(ret);
+        }),
+      );
   }
 }
