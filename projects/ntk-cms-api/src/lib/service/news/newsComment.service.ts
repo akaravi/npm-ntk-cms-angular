eac0c03147ab07048b22fd90a767d9048a5a684a
karavi home <offic-karavi@ntk.ir>
2020-11-16 19:08:44 +0330

    NewsComment

--- projects/ntk-cms-api/src/lib/service/news/newsComment.service.ts
+++ projects/ntk-cms-api/src/lib/service/news/newsComment.service.ts
@@ -1,11 +1,43 @@
 import { ApiCmsServerBase } from '../base/apiCmsServerBase.service';
 import { NewsCommentModel } from '../../models/entity/news/newsCommentModel';
+import { ErrorExcptionResultBase } from 'ntk-cms-api';
+import { Observable } from 'rxjs';
+import { map, retry } from 'rxjs/operators';
 
 
 export class NewsCommentService extends ApiCmsServerBase<NewsCommentModel, number>  {
-
-
   getModuleCotrolerUrl(): string {
     return 'NewsComment';
+  }
+
+
+  ServiceLikeClick(Id: number): Observable<ErrorExcptionResultBase> {
+
+    return this.http
+      .get(this.getBaseUrl() + this.getModuleCotrolerUrl() + '/LikeClick/' + Id, {
+        headers: this.getHeaders(),
+      })
+      .pipe(
+        retry(this.configApiRetry),
+        // catchError(this.handleError)
+        map((ret: ErrorExcptionResultBase) => {
+          return this.errorExcptionResultBaseCheck(ret);
+        }),
+      );
+  }
+
+  ServiceDisLikeClick(Id: number): Observable<ErrorExcptionResultBase> {
+
+    return this.http
+      .get(this.getBaseUrl() + this.getModuleCotrolerUrl() + '/DisLikeClick/' + Id, {
+        headers: this.getHeaders(),
+      })
+      .pipe(
+        retry(this.configApiRetry),
+        // catchError(this.handleError)
+        map((ret: ErrorExcptionResultBase) => {
+          return this.errorExcptionResultBaseCheck(ret);
+        }),
+      );
   }
 }
