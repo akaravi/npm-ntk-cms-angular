237fd4037357957225394753f8ad26ff93309744
Alireza Karavi ML <34806399+akaravi@users.noreply.github.com>
2021-01-23 19:05:31 +0330

    model update

--- projects/ntk-cms-api/src/lib/models/entity/base/tokenInfoModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/base/tokenInfoModel.ts
@@ -3,27 +3,32 @@ import { EnumManageUserAccessControllerTypes } from '../../enums/enumManageUserA
 
 
 export class TokenInfoModel {
-  Token: string;
-  DeviceToken: string;
-  RefreshToken: string;
 
-  UserId: number;
-  SiteId: number;
-  UserGroupId: number;
-  UserTypeTitle: string;
-  UserAccessAdminAllowToProfessionalData: boolean;
-  UserAccessAdminAllowToAllData: boolean;
-  UserType: EnumManageUserAccessControllerTypes;
-  UserAccessAreaType: EnumManageUserAccessAreaTypes;
-  Username: string;
-  Name: string;
-  LastName: string;
-  FullName: string;
-  Language: string;
-  Domain: string;
-  SubDomain: string;
-  Title: string;
-  PhotoUrl: string;
-  MobileConfirmed: boolean;
-  EmailConfirmed: boolean;
+    Token: string;
+    DeviceToken: string;
+    RefreshToken: string;
+    TokenExpireDate: string;
+    SiteId: number;
+    UserId: number;
+    MemberId: number;
+    DeviceId: number;
+    ApplicationId: number;
+    UserGroupId: number;
+    UserTypeTitle: string;
+    UserAccessAdminAllowToProfessionalData: boolean;
+    UserAccessAdminAllowToAllData: boolean;
+    UserType: EnumManageUserAccessControllerTypes;
+    UserAccessAreaType: EnumManageUserAccessAreaTypes;
+    Username: string;
+    Name: string;
+    LastName: string;
+    FullName: string;
+    Language: string;
+    Domain: string;
+    SubDomain: string;
+    Title: string;
+    PhotoUrl: string;
+    MobileConfirmed: boolean;
+    EmailConfirmed: boolean;
+
 }
