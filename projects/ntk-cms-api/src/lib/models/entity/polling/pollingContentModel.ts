f109285e621bd5e1d2d8cc0d4668ab63f8386361
Alireza Karavi ML <34806399+akaravi@users.noreply.github.com>
2021-02-14 10:51:06 +0330

    polling model update

--- projects/ntk-cms-api/src/lib/models/entity/polling/pollingContentModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/polling/pollingContentModel.ts
@@ -3,19 +3,15 @@ import { PollingCategoryModel } from './pollingCategoryModel';
 import { PollingOptionModel } from './pollingOptionModel';
 
 export class PollingContentModel extends BaseModuleEntity<number> {
-  Source: string;
   ExpireDate?: Date;
   ViewStatisticsBeforeVote: boolean;
   ViewStatisticsAfterVote: boolean;
   // tslint:disable-next-line: variable-name
   virtual_Category: PollingCategoryModel;
   Category: PollingCategoryModel;
-  Body: string;
-  Description: string;
   FromDate?: string;
   Geolocationlatitude?: number;
   Geolocationlongitude?: number;
-  Keyword: string;
   LinkCategoryId?: number;
   LinkFileIds: string;
   LinkFilePodcastId?: number;
@@ -25,7 +21,6 @@ export class PollingContentModel extends BaseModuleEntity<number> {
   ScoreSumPercent: number;
   Title: string;
   ViewCount: number;
-  Favorited: boolean;
   LinkMainImageIdSrc: string;
   LinkFilePodcastIdSrc: string;
   LinkFileMovieIdSrc: string;
