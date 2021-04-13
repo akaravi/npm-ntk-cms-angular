a8be3d5cc2cbd9ce43d0b1c165ad71d6deb18b85
Alireza Karavi-ML <34806399+akaravi@users.noreply.github.com>
2021-04-13 08:25:06 +0430

    update model

--- projects/ntk-cms-api/src/lib/models/entity/coreMain/coreGuideModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/coreMain/coreGuideModel.ts
@@ -1,16 +1,17 @@
 import { BaseEntity } from '../base/baseEntity';
 
 
-export class CoreGuideModel  extends BaseEntity<number> {
-     Title: string;
-    BodyFa: string;
-    BodyEn: string;
-    BodyAr: string;
-    LinkFileIds: string;
-    LinkParentId?: number;
-    // tslint:disable-next-line: variable-name
-    virtual_Parent: CoreGuideModel;
-    Parent: CoreGuideModel;
-    Children: CoreGuideModel[];
+export class CoreGuideModel extends BaseEntity<number> {
+  Key: string;
+  Title: string;
+  BodyFa: string;
+  BodyEn: string;
+  BodyAr: string;
+  LinkFileIds: string;
+  LinkParentId?: number;
+  // tslint:disable-next-line: variable-name
+  virtual_Parent: CoreGuideModel;
+  Parent: CoreGuideModel;
+  Children: CoreGuideModel[];
 }
 
