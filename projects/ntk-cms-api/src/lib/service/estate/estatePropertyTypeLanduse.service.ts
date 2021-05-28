ae8c9210677fe0be92499c12e7ddb00572794c84
karavi_mac <karavi@ntk.ir>
2021-05-28 13:48:57 +0430

    fix bug

--- projects/ntk-cms-api/src/lib/service/estate/estatePropertyTypeLanduse.service.ts
+++ projects/ntk-cms-api/src/lib/service/estate/estatePropertyTypeLanduse.service.ts
@@ -1,7 +1,7 @@
 
 import { ApiCmsServerBase } from '../base/apiCmsServerBase.service';
 import { Injectable } from '@angular/core';
-import { EstatePropertyTypeLanduseModel } from '../../models/entity/estate/estatePropertyTypeLanduseLanduseModel';
+import { EstatePropertyTypeLanduseModel } from '../../models/entity/estate/estatePropertyTypeLanduseModel';
 
 @Injectable()
 export class EstatePropertyTypeLanduseService extends ApiCmsServerBase<EstatePropertyTypeLanduseModel, string>  {
