7bc946890768bea9d032c74c933188f1026ac38a
Alireza Karavi <karavi.alireza@gmail.com>
2021-05-28 13:45:07 +0430

    model update

--- projects/ntk-cms-api/src/lib/service/estate/estatePropertyType.service.ts
+++ projects/ntk-cms-api/src/lib/service/estate/estatePropertyType.service.ts
@@ -1,6 +1,7 @@
 
 import { ApiCmsServerBase } from '../base/apiCmsServerBase.service';
 import { Injectable } from '@angular/core';
+import { EstatePropertyTypeModel } from '../../models/entity/estate/estatePropertyTypeModel';
 
 @Injectable()
 export class EstatePropertyTypeService extends ApiCmsServerBase<EstatePropertyTypeModel, string>  {
