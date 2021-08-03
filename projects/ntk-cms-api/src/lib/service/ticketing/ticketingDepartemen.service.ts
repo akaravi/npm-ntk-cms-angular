7d0636dcbf4df0553e013b3b3048464fe0b5770f
Alireza Karavi <karavi.alireza@gmail.com>
2021-08-03 11:16:22 +0430

    origin getall add

--- projects/ntk-cms-api/src/lib/service/ticketing/ticketingDepartemen.service.ts
+++ projects/ntk-cms-api/src/lib/service/ticketing/ticketingDepartemen.service.ts
@@ -1,6 +1,12 @@
 import { TicketingDepartemenModel } from '../../models/entity/ticketing/ticketingDepartemenModel';
 import { ApiCmsServerBase } from '../base/apiCmsServerBase.service';
 import { Injectable } from '@angular/core';
+import { TicketingFaqModel } from '../../models/entity/ticketing/ticketingFaqModel';
+import { FilterModel } from '../../models/entity/base/filterModel';
+import { Observable } from 'rxjs';
+import { ErrorExceptionResult } from '../../models/entity/base/errorExceptionResult';
+import { map, retry } from 'rxjs/operators';
+
 
 
 @Injectable({
@@ -10,6 +16,21 @@ export class TicketingDepartemenService extends ApiCmsServerBase<TicketingDepart
   getModuleCotrolerUrl(): string {
     return 'TicketingDepartemen';
   }
-
+  ServiceGetAllOrigin( model: FilterModel): Observable<ErrorExceptionResult<TicketingDepartemenModel>> {
+    if (model == null) {
+      model = new FilterModel();
+    }
+    return this.http
+      .post(this.getBaseUrl() + this.getModuleCotrolerUrl() + '/GetAllOrigin/', model, {
+        headers: this.getHeaders(),
+      })
+      .pipe(
+        retry(this.configApiRetry),
+        // catchError(this.handleError)
+        map((ret: any) => {
+          return this.errorExceptionResultCheck(ret);
+        }),
+      );
+  }
 
 }
--- projects/ntk-cms-api/src/lib/service/ticketing/ticketingFaq.service.ts
+++ projects/ntk-cms-api/src/lib/service/ticketing/ticketingFaq.service.ts
@@ -1,6 +1,10 @@
 import { TicketingFaqModel } from '../../models/entity/ticketing/ticketingFaqModel';
 import { ApiCmsServerBase } from '../base/apiCmsServerBase.service';
 import { Injectable } from '@angular/core';
+import { FilterModel } from '../../models/entity/base/filterModel';
+import { Observable } from 'rxjs';
+import { ErrorExceptionResult } from '../../models/entity/base/errorExceptionResult';
+import { map, retry } from 'rxjs/operators';
 
 
 @Injectable({
@@ -11,5 +15,20 @@ export class TicketingFaqService extends ApiCmsServerBase<TicketingFaqModel, num
     return 'TicketingFaq';
   }
 
-
+  ServiceGetAllOrigin( model: FilterModel): Observable<ErrorExceptionResult<TicketingFaqModel>> {
+    if (model == null) {
+      model = new FilterModel();
+    }
+    return this.http
+      .post(this.getBaseUrl() + this.getModuleCotrolerUrl() + '/GetAllOrigin/', model, {
+        headers: this.getHeaders(),
+      })
+      .pipe(
+        retry(this.configApiRetry),
+        // catchError(this.handleError)
+        map((ret: any) => {
+          return this.errorExceptionResultCheck(ret);
+        }),
+      );
+  }
 }
