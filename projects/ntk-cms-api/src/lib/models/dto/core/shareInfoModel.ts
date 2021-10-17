ce0c3252504e0e133be6cbe95cc54d03edcc5eb5
Alireza Karavi <karavi.alireza@gmail.com>
2021-10-17 19:33:15 +0330

    update model

--- projects/ntk-cms-api/src/lib/models/dto/core/shareInfoModel.ts
+++ projects/ntk-cms-api/src/lib/models/dto/core/shareInfoModel.ts
@@ -2,6 +2,20 @@ export class ShareInfoModel {
   SiteTitle: string;
   UserTitle: string;
   UrlResellerUser: string;
+  UrlSiteHome: string;
+  UrlSiteCPanel: string;
   UrlResellerSite: string;
   UrlResellerSiteCategory: string;
+
+  UrlSiteHomeShortLinkUrl: string;
+  UrlSiteCPanelShortLinkUrl: string;
+  UrlResellerSiteShortLinkUrl: string;
+  UrlResellerUserShortLinkUrl: string;
+  UrlResellerSiteCategoryShortLinkUrl: string;
+
+  UrlSiteHomeShortLinkQRCodeBase64: string;
+  UrlSiteCPanelShortLinkQRCodeBase64: string;
+  UrlResellerSiteShortLinkQRCodeBase64: string;
+  UrlResellerUserShortLinkQRCodeBase64: string;
+  UrlResellerSiteCategoryShortLinkQRCodeBase64: string;
 }
