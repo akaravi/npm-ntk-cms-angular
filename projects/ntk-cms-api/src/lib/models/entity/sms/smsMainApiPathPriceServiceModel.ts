ea6f438d6862303af6a28587d77b67c0f84577ea
Alireza Karavi <karavi.alireza@gmail.com>
2021-10-30 17:55:01 +0330

    sms model update

--- projects/ntk-cms-api/src/lib/models/entity/sms/smsMainApiPathPriceServiceModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/sms/smsMainApiPathPriceServiceModel.ts
@@ -4,10 +4,12 @@ import { SmsMainApiPathModel } from './smsMainApiPathModel';
 export class SmsMainApiPathPriceServiceModel extends BaseModuleEntity<string> {
   LinkApiPathId: string;
   RegulatorNumber: string;
-  ServiceUnicodePrice: number;
-  ServiceNormalPrice: number;
-  UserUnicodePrice: number;
-  UserNormalPrice: number;
+  ServiceUnicodeMessagePrice: number;
+  ServiceNormalMessagePrice: number;
+  ServiceVoicePrice: number;
+  UserUnicodeMessagePrice: number;
+  UserNormalMessagePrice: number;
+  UserVoicePrice: number;
   // tslint:disable-next-line: variable-name
   virtual_ApiPath: SmsMainApiPathModel;
   ApiPath: SmsMainApiPathModel;
