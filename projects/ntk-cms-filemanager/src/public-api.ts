f6d257a0d6b4a1c274dfa6ea2d2c0317aa3a3f07
Alireza Karavi <karavi@ntk.ir>
2023-03-29 18:26:52 +0330

    indo

--- projects/ntk-cms-filemanager/src/public-api.ts
+++ projects/ntk-cms-filemanager/src/public-api.ts
@@ -4,3 +4,14 @@ export * from './lib/enums/download-mode.enum';
 export * from './lib/cms-filemanager.component';
 export * from './lib/cms-filemanager.module';
 export * from './lib/services/node.service';
+/**export child */
+export * from './lib/components/folder-content/folder-content.component';
+export * from './lib/components/tree/tree.component';
+export * from './lib/components/tree/node-lister/node-lister.component';
+export * from './lib/components/nav-bar/nav-bar.component';
+export * from './lib/components/navigation/navigation.component';
+export * from './lib/components/side-view/side-view.component';
+export * from './lib/components/functions/new-folder/new-folder.component';
+export * from './lib/components/functions/node/node.component';
+export * from './lib/components/functions/loading-overlay/loading-overlay.component';
+export * from './lib/components/functions/upload/upload.component';
