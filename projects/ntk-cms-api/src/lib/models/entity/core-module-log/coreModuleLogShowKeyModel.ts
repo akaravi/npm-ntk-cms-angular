84d1c0d639419b35650d7bf9a225c6d8bfa9854c
karavi <karavi@ntk.ir>
2023-05-25 20:57:00 +0330

    model fix bug

--- projects/ntk-cms-api/src/lib/models/entity/core-module-log/coreModuleLogShowKeyModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/core-module-log/coreModuleLogShowKeyModel.ts
@@ -4,6 +4,7 @@ export class CoreModuleLogShowKeyModel extends BaseModuleEntity<string> {
   moduleName: string;
   moduleEntityName: string;
   moduleEntityId: string;
-  subjectBody: string;
+  subjectTitle: string;
   expireDateTime: Date;
+  showKey:number;
 }
