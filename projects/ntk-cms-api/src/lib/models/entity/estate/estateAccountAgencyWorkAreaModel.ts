65769f5bd125d45dbb63c8a0908438845978df50
pipeline <pipeline@ntk.ir>
2023-06-12 14:15:23 +0330

    fix model case

--- projects/ntk-cms-api/src/lib/models/entity/estate/estateAccountAgencyWorkAreaModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/estate/estateAccountAgencyWorkAreaModel.ts
@@ -3,5 +3,5 @@ import { BaseModuleEntity } from '../base/baseModuleEntity';
 //@@tag-Version-2212191
 export class EstateAccountAgencyWorkAreaModel extends BaseModuleEntity<string> {
   linkEstateAccountAgencyId: string;
-  LinkCoreLocationId: string;
+  linkCoreLocationId: string;
 }
--- projects/ntk-cms-api/src/lib/models/entity/estate/estateAccountUserWorkAreaModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/estate/estateAccountUserWorkAreaModel.ts
@@ -2,7 +2,7 @@
 import { BaseModuleEntity } from '../base/baseModuleEntity';
 //@@tag-Version-2212191
 export class EstateAccountUserWorkAreaModel extends BaseModuleEntity<string> {
-  LinkCoreLocationId: number;
+  linkCoreLocationId: number;
   linkEstateAccountUserId: string;
 
 }
