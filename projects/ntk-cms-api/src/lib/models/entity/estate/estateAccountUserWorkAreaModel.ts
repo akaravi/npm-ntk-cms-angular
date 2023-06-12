98e530759d1b8186b31e571dd395a121d7fa525b
pipeline <pipeline@ntk.ir>
2023-06-12 14:35:23 +0330

    fix bug

--- projects/ntk-cms-api/src/lib/models/entity/estate/estateAccountUserWorkAreaModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/estate/estateAccountUserWorkAreaModel.ts
@@ -2,7 +2,6 @@
 import { BaseModuleEntity } from '../base/baseModuleEntity';
 //@@tag-Version-2212191
 export class EstateAccountUserWorkAreaModel extends BaseModuleEntity<string> {
-  linkCoreLocationId: number;
   linkEstateAccountUserId: string;
-
+  linkCoreLocationId: number;
 }
