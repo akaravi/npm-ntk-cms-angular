8eec22b54022d028116cdc6fb243b2b975f8ffab
pipeline <pipeline@ntk.ir>
2023-07-15 15:04:58 +0330

    model update

--- projects/ntk-cms-api/src/lib/models/dto/estate/estateAccountUserFilterModel.ts
+++ projects/ntk-cms-api/src/lib/models/dto/estate/estateAccountUserFilterModel.ts
@@ -1,6 +1,8 @@
 import { FilterModel } from "../../entity/base/filterModel";
 
 export class EstateAccountUserFilterModel extends FilterModel{
+  onDateTimeFrom: Date;
+  onDateTimeTo: Date;
   locationListIds:number[];
   linkLocationWorkAreaIds:number[];
 }
