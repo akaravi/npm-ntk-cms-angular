0cf0d903aa6b82306e43cca49093c640482e9c96
Alireza Karavi <karavi.alireza@gmail.com>
2023-08-01 16:14:31 +0330

    update model notifiction

--- projects/ntk-cms-api/src/lib/models/dto/core-main/cmsNotificationSendDtoModel.ts
+++ projects/ntk-cms-api/src/lib/models/dto/core-main/cmsNotificationSendDtoModel.ts
@@ -1,10 +1,16 @@
+import { EnumManageUserAccessAreaTypes } from "../../enums/base/enumManageUserAccessAreaTypes";
+import { EnumManageUserAccessUserTypes } from "../../enums/base/enumManageUserAccessUserTypes";
+import { EnumNotificationConnectionType } from "../../enums/base/enumNotificationConnectionType";
 import { CmsNotificationModel } from "./cmsNotificationModel";
 
 export class CmsNotificationSendDtoModel extends CmsNotificationModel {
+  sendToConnectionTypes:EnumNotificationConnectionType[]
   sendToAll: boolean;
   sendToLinkSiteIds: number[];
   sendToLinkAppIds: number[];
   sendToLinkUserIds: number[];
   sendToLinkTokenNotificationIds: string[];
   sendToDeviceIds: string[];
+  SendToUserTypes:EnumManageUserAccessUserTypes[]
+  SendToAreaTypes:EnumManageUserAccessAreaTypes[]
 }
