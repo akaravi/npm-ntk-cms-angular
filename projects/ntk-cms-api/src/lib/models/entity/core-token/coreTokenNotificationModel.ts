6c42491512fc7f9b2acf515547c3f586fe30c5e7
karavi <karavi@ntk.ir>
2023-08-04 16:37:16 +0330

    fix bug

--- projects/ntk-cms-api/src/lib/models/entity/core-token/coreTokenNotificationModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/core-token/coreTokenNotificationModel.ts
@@ -1,24 +1,24 @@
 import { NotificationTypeEnum } from '../../enums/base/notificationTypeEnum';
 import { ManageUserAccessUserTypesEnum } from '../../enums/base/manageUserAccessUserTypesEnum';
-import { NotificationConnectionTypeEnum } from '../../enums/base/notificationConnectionTypeEnum1';
+import { NotificationConnectionTypeEnum } from '../../enums/base/notificationConnectionTypeEnum';
 import { NotificationConnectionStatusEnum } from '../../enums/base/notificationConnectionStatusEnum';
 import { BaseEntity } from '../base/baseEntity';
 import { ManageUserAccessAreaTypesEnum } from '../../enums/base/manageUserAccessAreaTypesEnum';
 //@@tag-Version-2201011
 export class CoreTokenNotificationModel extends BaseEntity<string>   {
-    linkUserId?: number;
-    linkSiteId?: number;
-    linkAppId?: number;
-    userToken: string;
-    connectionId: string;
-    deviceId: string;
-    deviceType: NotificationTypeEnum;
-    userType: ManageUserAccessUserTypesEnum;
-    areaType: ManageUserAccessAreaTypesEnum;
-    deviceConnectionType: NotificationConnectionTypeEnum;
-    deviceConnectionState: NotificationConnectionStatusEnum;
+  linkUserId?: number;
+  linkSiteId?: number;
+  linkAppId?: number;
+  userToken: string;
+  connectionId: string;
+  deviceId: string;
+  deviceType: NotificationTypeEnum;
+  userType: ManageUserAccessUserTypesEnum;
+  areaType: ManageUserAccessAreaTypesEnum;
+  deviceConnectionType: NotificationConnectionTypeEnum;
+  deviceConnectionState: NotificationConnectionStatusEnum;
 
-    usedUsername: string;
-    iP: string;
-    description: string;
+  usedUsername: string;
+  iP: string;
+  description: string;
 }
