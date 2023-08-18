3dc7e01760da26019cc732b7d1323fa9983c9efb
karavi <karavi@ntk.ir>
2023-08-18 13:31:50 +0330

    model update

--- projects/ntk-cms-api/src/lib/models/entity/core-main/file360TourModel.ts
+++ projects/ntk-cms-api/src/lib/models/entity/core-main/file360TourModel.ts
@@ -1,7 +1,7 @@
 import { File360TourDefaultModel } from "./file360TourDefaultModel";
 import { File360TourScenesModel } from "./file360TourScenesModel";
 
-export class File360TourModel{
-  Default:File360TourDefaultModel;
-  scenes:Map<string,File360TourScenesModel>;
+export class File360TourModel {
+  defaultValue: File360TourDefaultModel;
+  scenes: Map<string, File360TourScenesModel>;
 }
