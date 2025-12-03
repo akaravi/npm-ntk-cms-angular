0a7a30bdf80db6853f93ebe5df1064796c4c373c
karavi <karavi.ml@ntk.co>
2025-12-03 13:29:26 +0330

    update todo model

--- projects/ntk-cms-api/src/lib/models/dto/sms/smsApiSendMessageDtoModel.ts
+++ projects/ntk-cms-api/src/lib/models/dto/sms/smsApiSendMessageDtoModel.ts
@@ -1,15 +1,15 @@
 
 export class SmsApiSendMessageDtoModel {
-  checkProcesses:boolean=false;
   linkApiPathId: string;
   linkFromNumber: string;
   toNumbers: string;
   toContactCategories: string[];
   toContactContents: string[];
   message: string;
-  sendByQueue: boolean;
-  isFlash: boolean;
   scheduleSendStart: Date;
   scheduleSendExpire: Date;
   scheduleCron: string;
+  optionTypes: string;
+  optionSendByQueue: boolean;
+  optionCheckProcesses: boolean;
 }
